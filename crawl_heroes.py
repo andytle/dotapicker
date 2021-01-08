8f2020d6d927c96fbd80c0c772acf02bb835f5a9
Andy Le <43190181+andytle@users.noreply.github.com>
2021-01-08 18:17:35 +0700

    Add files via upload

---
+++ crawl_heroes.py
@@ -0,0 +1,27 @@
+# from PIL import ImageGrab
+# from datetime import datetime
+
+from bs4 import BeautifulSoup
+import requests
+
+def crawl_heroes(heroLink):
+    link = heroLink.get("href")
+    heroPage = requests.get(link)
+    heroSoup = BeautifulSoup(heroPage.content, 'html.parser')
+    heroName = heroSoup.find("h1").text
+    heroPortrait = heroSoup.find("img", {"id": "heroTopPortraitIMG"}).get("src")
+    get_image(heroName, heroPortrait)
+
+def get_image(hero, image_url):
+    print("Getting img for " + hero)
+    img_data = requests.get(image_url).content
+    with open(hero + ".jpg", 'wb') as handler:
+        handler.write(img_data)
+
+if __name__ == "__main__":
+    page = requests.get("https://www.dota2.com/heroes/")
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    heroLinks = soup.findAll("a", {"class": "heroPickerIconLink"})
+    for heroLink in heroLinks:
+        crawl_heroes(heroLink)
